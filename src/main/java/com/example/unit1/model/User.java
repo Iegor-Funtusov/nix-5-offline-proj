dcc65e8d2b8cbbd845fbcb6d5eda2a06ba28feca
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-04-22 23:53:27 +0300

    add User model and User DAO

---
+++ src/main/java/com/example/unit1/model/User.java
@@ -0,0 +1,12 @@
+package com.example.unit1.model;
+
+import libs.lombok.*;
+
+@Data
+public class User{
+    
+    private String id;
+    private String firstName;
+    private String lastName;
+
+}
