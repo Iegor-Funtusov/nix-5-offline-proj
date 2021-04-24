759f529f92f83ca34d924f83226b3f8e006b4197
rslnmzk <ayayron1921@gmail.com>
2021-04-24 23:11:06 +0300

    final version

--- arithmetic/src/Array_class.java
+++ arithmetic/src/Array_class.java
@@ -6,9 +6,16 @@ public class Array_class {
     public static void main(String[] args) throws IOException {
         Array_class arrayClassObj = new Array_class();
 
+        arrayClassObj.first(arrayClassObj.readArr());
+        arrayClassObj.second(arrayClassObj.readArr());
+        arrayClassObj.third(arrayClassObj.readArr());
+        arrayClassObj.fourth(arrayClassObj.readArr());
+        arrayClassObj.fifth(arrayClassObj.readArr());
+        arrayClassObj.sixth(arrayClassObj.readArr());
+    }
+
+    public int[] readArr() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите длину массива: ");
-        int numForInput = Integer.parseInt(reader.readLine());
         System.out.print("Введите числа через Space и нажмите Enter: ");
         String lines = reader.readLine();
         String[] strs = lines.trim().split("\\s+");
@@ -16,13 +23,7 @@ public class Array_class {
         for (int i = 0; i < strs.length; i++) {
             array[i] = Integer.parseInt(strs[i]);
         }
-
-        arrayClassObj.first(array);
-        arrayClassObj.second(array);
-        arrayClassObj.third(array);
-        arrayClassObj.fourth(array);
-        arrayClassObj.fifth(array);
-
+        return array;
     }
 
     public void first(int[] array) {
@@ -70,14 +71,26 @@ public class Array_class {
 
     public void fifth(int[] array) {
         System.out.println("Task 5");
-        for (int i = 0; i < array.length/2; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int thirdValue = array[i];
-            array[i] = array[5 - i - 1];
-            array[5 - i - 1] = thirdValue;
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = thirdValue;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void sixth(int[] array) {
+        System.out.println("Task 6");
+        for (int i = 0; i < array.length - 1; i += 2) {
+            int thirdValue = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = thirdValue;
         }
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
     }
-
 }
