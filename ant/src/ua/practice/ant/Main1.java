3ee933a392e90e506d22f2c5d0b2a8f452f12c41
IKrasnienkov <krasnenkow56@gmail.com>
2021-04-24 21:53:20 +0300

    feat: build project with console/ant/maven

---
+++ ant/src/ua/practice/ant/Main1.java
@@ -0,0 +1,8 @@
+package ua.practice.ant;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Main1.main");
+        new Class1().print();
+    }
+}
---
+++ maven.bat
@@ -0,0 +1,7 @@
+@echo off
+set MAVEN_OPTS=-Xmx2G -Dfile.encoding=UTF-8
+set MAVEN_HOME=%~dp0maven\apache-maven-3.8.1
+set PATH=%MAVEN_HOME%\bin;%PATH%
+rem deleting CLASSPATH as a workaround for PLA-8702
+set CLASSPATH=
+mvn -version & cd maven & cd app & mvn clean install & java -jar target/app-1.0-SNAPSHOT.jar
