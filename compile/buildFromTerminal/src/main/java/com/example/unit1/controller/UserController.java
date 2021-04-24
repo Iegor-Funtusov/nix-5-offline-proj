2a3bd4b938868a2ef64124bf18dba90a54d1084b
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-04-24 21:33:42 +0300

    fix clean scripts

--- compile/buildFromTerminal/src/main/java/com/example/unit1/controller/UserController.java
+++ compile/buildFromTerminal/src/main/java/com/example/unit1/controller/UserController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import main.java.com.example.unit1.model.User;
 import main.java.com.example.unit1.service.UserService;
 import main.java.com.example.unit1.service.UserServiceImpl;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,21 +19,22 @@ public class UserController {
     public void exec(){
         try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in))){
             String read;
-            System.out.println("Set an action: " +"\n 1 - insert User" +"\n 2 - update User"
-                    + "\n 3 - delete User" + "\n 4 - get all users" + "\n 0 - exit");
+            String helpStr = "Set an action: " +"\n 1 - insert User" +"\n 2 - update User"
+                    + "\n 3 - delete User" + "\n 4 - get all users" + "\n 0 - exit";
+            System.out.println(StringUtils.upperCase(helpStr));
             while((read = in.readLine()) != null){
                 switch (read) {
                     case "1":{
-                        insert(in);
+                        createUser(in);
                     } break;
                     case "2":{
-                        update(in);
+                        updateUser(in);
                     } break;
                     case "3":{
-                        delete(in);
+                        deleteUserById(in);
                     } break;
                     case "4":{
-                        getAll();
+                        getAllUsersAndPrint();
                     } break;
                     case "0":{
                         System.exit(0);
@@ -46,7 +48,7 @@ public class UserController {
 
     }
 
-    private void insert(BufferedReader reader) throws IOException {
+    private void createUser(BufferedReader reader) throws IOException {
         User user = new User();
         System.out.println("Enter first name of user: ");
         user.setFirstName(reader.readLine());
@@ -55,7 +57,7 @@ public class UserController {
         userService.create(user);
     }
 
-    private void update(BufferedReader reader) throws IOException {
+    private void updateUser(BufferedReader reader) throws IOException {
         User user = new User();
         System.out.println("Enter Id of user which you want to change: ");
         user.setId(reader.readLine());
@@ -65,12 +67,12 @@ public class UserController {
         user.setLastName(reader.readLine());
         userService.update(user);
     }
-    private void delete(BufferedReader reader) throws IOException {
+    private void deleteUserById(BufferedReader reader) throws IOException {
         System.out.println("Enter Id of user which you want to Delete: ");
         String id = reader.readLine();
         userService.deleteById(id);
     }
-    private void getAll() throws IOException {
+    private void getAllUsersAndPrint() {
         List<User> users = userService.getAllUsers();
         String usersJson = gson.toJson(users);
         System.out.println(usersJson);
--- compile/buildFromTerminal/src/main/java/com/example/unit1/model/User.java
+++ compile/buildFromTerminal/src/main/java/com/example/unit1/model/User.java
@@ -1,14 +1,32 @@
 package main.java.com.example.unit1.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class User {
     
     private String id;
     private String firstName;
     private String lastName;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
