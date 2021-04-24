bc366a690d07dbe83395d2e9652f7e8a09e8656a
Nikita <logachovnik@gmail.com>
2021-04-24 18:36:21 +0300

    changed project structure, add class Print, add file with commands

--- unit_1_compile/src/Main.java
+++ unit_1_compile/src/Main.java
@@ -1,5 +1,8 @@
+import print.Print;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Print print = new Print();
+        print.print("Hello");
     }
 }
---
+++ unit_1_compile/src/print/Print.java
@@ -0,0 +1,9 @@
+package print;
+
+public class Print {
+    private String message = "World";
+
+    public void print(String str){
+        System.out.println(str + this.message);
+    }
+}
