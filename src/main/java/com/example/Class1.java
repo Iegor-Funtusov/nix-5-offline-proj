d8fdebab1ddfcfacc4fa7a290c3118bf8036d748
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-04-25 14:31:20 +0300

    Update build.xml

--- src/main/java/com/example/Class1.java
+++ src/main/java/com/example/Class1.java
@@ -1,17 +1,9 @@
 package com.example;
 
-//import org.apache.commons.collections4.CollectionUtils;
-//import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Class1 {
 
     public static void main(String[] args) {
         System.out.println("Hello from JDK build! Class1");
-//        System.out.println(StringUtils.capitalize("apache commons lang3 library"));
-//        List<String> list = Arrays.asList("a", "b", "c");
-//        System.out.println(CollectionUtils.isEmpty(list));
     }
 }
