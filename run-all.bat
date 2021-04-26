5d92919abf5ce0282d8268ed5db775d4d6a1e0f9
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-04-26 09:24:47 +0300

    Update run-all.bat

--- run-all.bat
+++ run-all.bat
@@ -1,14 +1,10 @@
 echo 1
-javac src\main\java\com\example\Class1.java
-cd src\main\java
-java com.example.Class1
-
+javac -sourcepath src\ -cp libs\commons-collections4-4.4.jar:libs\commons-lang3-3.12.0.jar src\main\java\com\example\Class1.java
+java -cp libs\commons-collections4-4.4.jar:libs\commons-lang3-3.12.0.jar -cp src\main\java com.example.Class1
 
 echo 2
-cd ..\..\..\
 call ant
 java -jar myRunnableJAR.jar
-
 
 echo 3
 call mvn clean package
