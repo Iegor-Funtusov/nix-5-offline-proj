44e9b5e8f7170db8dc10355195b4d2acbee60a0b
rslnmzk <ayayron1921@gmail.com>
2021-04-30 10:55:26 +0300

    сделал коня

--- arrays/src/Knight.java
+++ arrays/src/Knight.java
@@ -1,6 +1,18 @@
 public class Knight extends ChessPiece {
     @Override
     public boolean canMove(Coordinates to) {
-        return false;
+        if((to.x != this.position.x + 1 && to.x != this.position.x - 1) && (to.x != this.position.x + 2 && to.x != this.position.x - 2)){
+            return false;
+        }
+        if ((this.color == Color.WHITE) || (this.color == Color.BLACK)) {
+            if ((to.y != position.y + 2 && to.y != position.y - 2) && (to.y != position.y + 1 && to.y != position.y - 1)) {
+                return false;
+            }
+        } else {
+            if ((to.y != position.y - 2 && to.y != position.y + 2) && (to.y != position.y - 1 && to.y != position.y + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
