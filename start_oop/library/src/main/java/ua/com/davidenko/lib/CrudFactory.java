5f4dcc331126672c2b52024976f5f17cdc30a2db
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-05-07 13:56:31 +0300

    add classes for library

---
+++ start_oop/library/src/main/java/ua/com/davidenko/lib/CrudFactory.java
@@ -0,0 +1,41 @@
+package ua.com.davidenko.lib;
+
+import org.reflections.Reflections;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+public class CrudFactory {
+
+    private static CrudFactory instance;
+    private static final String ROOT_PACKAGE = "ua.com.davidenko.lib";
+    private Set<Class<? extends CrudService>> crudServiceList;
+
+    private CrudFactory() {
+        Reflections reflections = new Reflections(ROOT_PACKAGE);
+        crudServiceList = reflections.getSubTypesOf(CrudService.class);
+    }
+
+    public static CrudFactory getInstance() {
+        if (instance == null) {
+            instance = new CrudFactory();
+        }
+        return instance;
+    }
+
+    public CrudService getCurrent(String crudName) {
+        for (Class<? extends CrudService> aClass : crudServiceList) {
+            if (aClass.getSimpleName().equalsIgnoreCase(crudName)) {
+                try {
+                    System.out.println(aClass.getSimpleName());
+                    return aClass.getDeclaredConstructor().newInstance();
+                } catch (InstantiationException |
+                        IllegalAccessException |
+                        InvocationTargetException |
+                        NoSuchMethodException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        throw new RuntimeException("some Exeption");
+    }
+}
