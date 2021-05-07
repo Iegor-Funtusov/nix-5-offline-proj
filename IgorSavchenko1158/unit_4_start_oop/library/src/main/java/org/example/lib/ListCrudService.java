8695a7ea3315207a4350ad197a1a00c87e54c2dd
unknown <igor.savchenko1158@gmail.com>
2021-05-07 15:36:03 +0300

    Deprecate ListCrudService

--- IgorSavchenko1158/unit_4_start_oop/library/src/main/java/org/example/lib/ListCrudService.java
+++ IgorSavchenko1158/unit_4_start_oop/library/src/main/java/org/example/lib/ListCrudService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-
+@Deprecated
 public class ListCrudService<E extends BaseEntity> implements CrudService<E> {
 
     List<E> container = new ArrayList<>();
