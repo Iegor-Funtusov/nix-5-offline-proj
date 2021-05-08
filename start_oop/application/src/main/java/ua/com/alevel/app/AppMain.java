4130853d4c7e151f55f5b2c4853fb73ec31530b3
pilyha <illia.popov57@gmail.com>
2021-05-08 21:36:15 +0300

    Deleted comment

--- start_oop/application/src/main/java/ua/com/alevel/app/AppMain.java
+++ start_oop/application/src/main/java/ua/com/alevel/app/AppMain.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// SOLID
 public class AppMain {
 
     public static void main(String[] args) throws IOException {
