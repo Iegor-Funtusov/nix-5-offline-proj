0071901303fe89de97f515f47dc6f7bc64ef79bb
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-05-09 18:07:08 +0300

    add Main

---
+++ start_oop/application/src/main/java/ua/com/davidenko/app/AppStartMain.java
@@ -0,0 +1,9 @@
+package ua.com.davidenko.app;
+
+import java.io.IOException;
+
+public class AppStartMain {
+    public static void main(String[] args) throws IOException {
+       UserControl.userControlChoose();
+    }
+}
