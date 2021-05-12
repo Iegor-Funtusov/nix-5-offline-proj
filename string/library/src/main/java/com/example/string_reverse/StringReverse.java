82051d41253bed207535ac54c0444f927e05199e
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-05-12 23:58:36 +0300

    add reverse from start index to end index, reverse substring in string

--- string/library/src/main/java/com/example/string_reverse/StringReverse.java
+++ string/library/src/main/java/com/example/string_reverse/StringReverse.java
@@ -1,6 +1,15 @@
 package com.example.string_reverse;
 
 public class StringReverse {
+    public static String reverse(String src, int firstIndex, int lastIndex){
+        String destSubstring = src.substring(firstIndex, lastIndex);
+        return reverse(src,destSubstring);
+    }
+
+    public static String reverse(String src, String dest){
+        return src.replace(dest, reverse(dest));
+    }
+
     public static String reverse(String src){
         char[] srcChars = src.toCharArray();
         for (int i = 0; i < srcChars.length/2; i++) {
@@ -9,13 +18,5 @@ public class StringReverse {
             srcChars[srcChars.length-i-1] = tmp;
         }
         return String.valueOf(srcChars);
-    }
-
-    public static String reverse(String src, String dest){
-        return null;
-    }
-
-    public static String reverse(String src, int firstIndex, int lastIndex){
-        return null;
     }
 }
