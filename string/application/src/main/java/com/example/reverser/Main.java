1729349cb8126d2701a832ac2c3a4047a0ab357d
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-05-13 02:27:25 +0300

    add app, for test lib that reverse the string

--- string/application/src/main/java/com/example/reverser/Main.java
+++ string/application/src/main/java/com/example/reverser/Main.java
@@ -1,6 +1,14 @@
 package com.example.reverser;
 
+import com.example.string_reverse.StringReverse;
+
 public class Main {
     public static void main(String[] args) {
+        String helloStr = "hello world";
+        System.out.println(StringReverse.reverse(helloStr));
+        System.out.println(StringReverse.reverse(helloStr, "worl"));
+        System.out.println(StringReverse.reverse(helloStr,3,8));
+        System.out.println(StringReverse.reverse(helloStr, 'h','w'));
+        System.out.println(StringReverse.reverse(helloStr, "el", "ld"));
     }
 }
