c028cc483d1e5f5e5fa29eec92e6f030f5fdc1f0
unknown <igor.savchenko1158@gmail.com>
2021-05-15 17:11:01 +0300

    Finish task

--- IgorSavchenko1158/unit_5_string/application/src/main/java/org/example/Main.java
+++ IgorSavchenko1158/unit_5_string/application/src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ReverseStringUtils.reverse("Hello", "", "");
-
         Scanner scanner;
         String currentString;
         while (true) {
@@ -14,33 +13,75 @@ public class Main {
             currentString = scanner.nextLine();
 
             System.out.println("Choose operation: \n" +
+                    "0 -exit \n" +
                     "1 -reverse \n" +
-                    "2 -reverse substring \n" +
-                    "3 -reverse bound by index \n" +
-                    "4 -reverse bound by character");
+                    "2 -reverse bound by index \n" +
+                    "3 -reverse bound by alphanumeric characters, inclusive \n" +
+                    "4 -reverse bound by substrings, inclusive \n" +
+                    "5 -reverse substring \n" +
+                    "6 -reverse substring bound by index \n" +
+                    "7 -reverse substring bound by alphanumeric characters, inclusive \n" +
+                    "8 -reverse substring bound by substrings, inclusive");
 
-            switch (scanner.nextLine()) {
-                case "1": {
-                    System.out.println(ReverseStringUtils.reverse(currentString));
-                    break;
-                }
-                case "2": {
-                    System.out.println("Enter substring:");
-                    System.out.println(ReverseStringUtils.reverseSubstring(currentString, scanner.nextLine()));
-                    break;
-                }
-                case "3": {
-                    System.out.println("Enter indexes split by space:");
-                    System.out.println(ReverseStringUtils.reverse(currentString, scanner.nextInt(), scanner.nextInt()));
-                    break;
-                }
-                case "4": {
-                    System.out.println("Enter start and end characters split by space:");
-                    System.out.println(ReverseStringUtils.reverse(currentString, scanner.next().charAt(0), scanner.next().charAt(0)));
-                    break;
-                }
-                default: return;
+            try {
+                switcher(scanner, currentString);
+            } catch (IllegalArgumentException | InputMismatchException ex) {
+                System.out.println("Wrong input");
             }
+        }
+    }
+
+    private static void switcher(Scanner scanner, String currentString) {
+        switch (scanner.nextLine()) {
+            case "1": {
+                System.out.println(ReverseStringUtils.reverse(currentString));
+                break;
+            }
+            case "2": {
+                System.out.println("Enter indexes split by space:");
+                System.out.println(ReverseStringUtils.reverse(currentString, scanner.nextInt(), scanner.nextInt()));
+                break;
+            }
+            case "3": {
+                System.out.println("Enter start and end characters split by space:");
+                System.out.println(ReverseStringUtils.reverse(currentString, scanner.next().charAt(0), scanner.next().charAt(0)));
+                break;
+            }
+            case "4": {
+                System.out.println("Enter start and end substring split by linebreak:");
+                System.out.println(ReverseStringUtils.reverse(currentString, scanner.nextLine(), scanner.nextLine()));
+            }
+            case "5": {
+                System.out.println("Enter substring:");
+                System.out.println(ReverseStringUtils.reverseSubstring(currentString, scanner.nextLine()));
+                break;
+            }
+            case "6": {
+                System.out.println("Enter substring:");
+                String substring = scanner.nextLine();
+                System.out.println("Enter indexes split by space:");
+                System.out.println(ReverseStringUtils.reverseSubstring(currentString, substring, scanner.nextInt(), scanner.nextInt()));
+                break;
+            }
+            case "7": {
+                System.out.println("Enter substring:");
+                String substring = scanner.nextLine();
+                System.out.println("Enter start and end characters split by space:");
+                System.out.println(ReverseStringUtils.reverseSubstring(currentString, substring, scanner.next().charAt(0), scanner.next().charAt(0)));
+                break;
+            }
+            case "8": {
+                System.out.println("Enter substring:");
+                String substring = scanner.nextLine();
+                System.out.println("Enter start and end substring split by linebreak:");
+                System.out.println(ReverseStringUtils.reverseSubstring(currentString, substring, scanner.nextLine(), scanner.nextLine()));
+                break;
+            }
+            case "0": {
+                System.exit(0);
+            }
+            default:
+                break;
         }
     }
 }
--- IgorSavchenko1158/unit_5_string/reverse-string/src/main/java/org/example/ReverseStringUtils.java
+++ IgorSavchenko1158/unit_5_string/reverse-string/src/main/java/org/example/ReverseStringUtils.java
@@ -3,9 +3,7 @@ package org.example;
 public class ReverseStringUtils {
 
     public static String reverse(String src) {
-        if (src == null) {
-            throw new IllegalArgumentException();
-        }
+        if (src == null) throw new IllegalArgumentException();
 
         char[] chars = src.toCharArray();
 
@@ -25,37 +23,59 @@ public class ReverseStringUtils {
      * @param lastIndex  inclusive
      */
     public static String reverse(String src, int firstIndex, int lastIndex) {
-        if (src == null || firstIndex > lastIndex || lastIndex >= src.length()) {
+        if (src == null || firstIndex < 0 || firstIndex > lastIndex || lastIndex >= src.length())
             throw new IllegalArgumentException();
-        }
+
         return src.substring(0, firstIndex) +
                 reverse(src.substring(firstIndex, lastIndex + 1)) +
                 src.substring(lastIndex + 1);
     }
 
-    public static String reverse(String src, char firstChar, char lastChar) {
-        if (src == null) {
-            throw new IllegalArgumentException();
-        }
+    public static String reverse(String src, char startChar, char endChar) {
+        if (src == null) throw new IllegalArgumentException();
 
-        int indexOfFirstChar = src.indexOf(firstChar);
-        int indexOfLastChar = indexOfFirstChar + src.substring(indexOfFirstChar).indexOf(lastChar);
-        if (indexOfFirstChar < 0 || indexOfLastChar - indexOfFirstChar < 0) {
-            throw new IllegalArgumentException();
-        }
-        return reverse(src, indexOfFirstChar, indexOfLastChar);
+        return reverse(src, String.valueOf(startChar), String.valueOf(endChar));
     }
 
     public static String reverse(String src, CharSequence firstCS, CharSequence lastCS) {
-        return null;
+        if (src == null || firstCS == null || lastCS == null) throw new IllegalArgumentException();
+
+        int firstIndex = src.indexOf(firstCS.toString());
+        if (firstIndex < 0) throw new IllegalArgumentException();
+        int lastIndex = firstIndex + src.substring(firstIndex).indexOf(lastCS.toString()) + lastCS.length() - 1;
+        if (lastIndex - firstIndex < 0) throw new IllegalArgumentException();
+
+        return reverse(src, firstIndex, lastIndex);
     }
 
     public static String reverseSubstring(String src, String substring) {
-        if (src == null || substring == null) {
-            throw new IllegalArgumentException();
-        }
+        if (src == null || substring == null) throw new IllegalArgumentException();
         return src.replaceAll(substring, reverse(substring));
     }
 
+    public static String reverseSubstring(String src, String substring, int firstIndex, int lastIndex) {
+        if (src == null || substring == null || firstIndex < 0 || firstIndex > lastIndex || lastIndex >= src.length())
+            throw new IllegalArgumentException();
 
+        return src.substring(0, firstIndex) +
+                reverseSubstring(src.substring(firstIndex, lastIndex + 1), substring) +
+                src.substring(lastIndex + 1);
+    }
+
+    public static String reverseSubstring(String src, String substring, char startChar, char endChar) {
+        if (src == null || substring == null) throw new IllegalArgumentException();
+
+        return reverseSubstring(src, substring, String.valueOf(startChar), String.valueOf(endChar));
+    }
+
+    public static String reverseSubstring(String src, String substring, CharSequence firstCS, CharSequence lastCS) {
+        if (src == null || substring == null || firstCS == null || lastCS == null) throw new IllegalArgumentException();
+
+        int firstIndex = src.indexOf(firstCS.toString());
+        if (firstIndex < 0) throw new IllegalArgumentException();
+        int lastIndex = firstIndex + src.substring(firstIndex).indexOf(lastCS.toString()) + lastCS.length() - 1;
+        if (lastIndex - firstIndex < 0) throw new IllegalArgumentException();
+
+        return reverseSubstring(src, substring, firstIndex, lastIndex);
+    }
 }
