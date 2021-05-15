a52b980f8e35ee2785c353e03dc0d05c0a05cdd0
rslnmzk <ayayron1921@gmail.com>
2021-05-15 20:18:02 +0300

    unit 5

--- reverse_string/string_library/src/main/java/ua/com/alevel/lib/ReverseString.java
+++ reverse_string/string_library/src/main/java/ua/com/alevel/lib/ReverseString.java
@@ -4,10 +4,10 @@ public class ReverseString {
 
     public String reverse(String input) {
         char[] inputArray = input.toCharArray();
-        for (int i = 0, j = input.length() - 1; i < j; i++, j--) {
+        for (int i = 0; i < inputArray.length / 2; i++) {
             char ch = inputArray[i];
-            inputArray[i] = inputArray[j];
-            inputArray[j] = ch;
+            inputArray[i] = inputArray[inputArray.length - i - 1];
+            inputArray[inputArray.length - i - 1] = ch;
         }
         String result = new String(inputArray);
         return result;
@@ -20,14 +20,14 @@ public class ReverseString {
     public String reverse(String input, int firstIndex, int secondIndex) {
         String firstString = input.substring(0, firstIndex);
         String secondString = input.substring(secondIndex + 1);
-        String newString = input.substring(firstIndex, secondIndex);
+        String newString = input.substring(firstIndex, secondIndex + 1);
         return firstString + reverse(newString) + secondString;
     }
 
     public String reverse(String input, char firstSymbol, char secondSymbol) {
         int firstIndex = input.indexOf(firstSymbol);
         int secondIndex = input.indexOf(secondSymbol);
-        String newString = input.substring(firstIndex, secondIndex);
+        String newString = input.substring(firstIndex, secondIndex + 1);
         String firstString = input.substring(0, firstIndex);
         String secondString = input.substring(secondIndex + 1);
         return firstString + reverse(newString) + secondString;
