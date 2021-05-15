257c77e28301b4cc2b55ed1c87b6c9b51375eab9
eugenesation <eugenesmirnov03@gmail.com>
2021-05-16 00:53:45 +0300

    unit_5_string_completed

--- string/apps/src/main/java/ua/com/nix/Main.java
+++ string/apps/src/main/java/ua/com/nix/Main.java
@@ -3,14 +3,51 @@ package ua.com.nix;
 import libs.ReverseString;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(ReverseString.reverse("hello"));
-        System.out.println(ReverseString.reverse("hello world", "worl"));
-
+        offer();
+        String read = reader.readLine();
+        while (!read.isEmpty()){
+            switch (read){
+                case "0": {
+                    System.exit(0);
+                }
+                case "1":
+                    System.out.println("Введите строку: ");
+                    String src = reader.readLine();
+                    System.out.println(ReverseString.stringReverse(src));
+                    break;
+                case "2":
+                    System.out.println("Введите строку: ");
+                    String src2 = reader.readLine();
+                    System.out.println("Введите подстроку: ");
+                    String dest = reader.readLine();
+                    System.out.println(ReverseString.substringReverse(src2, dest));
+                    break;
+                case "3":
+                    System.out.println("Введите строку: ");
+                    String src3 = reader.readLine();
+                    System.out.println("Введите начальный индекс: ");
+                    int firstIndex = Integer.parseInt(reader.readLine());
+                    System.out.println("Введите конечный индекс: ");
+                    int lastIndex = Integer.parseInt(reader.readLine());
+                    System.out.println(ReverseString.indexReverse(src3, firstIndex, lastIndex));
+                    break;
+                default:
+                    System.out.println("Ошибка. Попробуйте ещё раз");
+                    break;
+            }
+        }
     }
 
+    public static void offer() {
+        System.out.println("Введите что бы вы хотели сделать: \n 1 - обычный реверс; \n" +
+                " 2 - реверс по указанной подстроке в строке; \n 3 - весь предыдущий функционал + укажите " +
+                "индексы начала и конца реверса; \n 0 - выход из программы.");
+
+    }
 }
--- string/libra/src/main/java/libs/ReverseString.java
+++ string/libra/src/main/java/libs/ReverseString.java
@@ -1,28 +1,41 @@
 package libs;
 
 public class ReverseString {
-    public static String reverse(String src){
-        char[] array = src.toCharArray();
-        String result = "";
-        for (int i = array.length - 1; i >= 0; i--) {
-            result = result + array[i];
-        }
-        return result;
-    }
-    public static String reverse(String src, String dest) {
-        String result = "";
-        int indexOfDest = src.indexOf(dest);
-        String first = src.substring(0, indexOfDest);
-        String second = src.substring(indexOfDest);
-        char[] array = second.toCharArray();
-            for (int i = array.length - 1; i >= 0; i--) {
-            result = result + array[i];
-            }
-        first = first + result;
-        return first;
-    }
-    public static String reverse(String src, int firstIndex, int lastIndex){
 
-        return null;
+    public static String stringReverse(String src){
+        if (!src.isEmpty()) {
+            char[] array = src.toCharArray();
+            String result = "";
+            for (int i = array.length - 1; i >= 0; i--) {
+                result = result + array[i];
+            }
+            return result;
+        }
+        return "Попоробуйте ещё раз";
+    }
+    public static String substringReverse(String src, String dest) {
+        if(src.contains(dest) && !src.isEmpty() && !dest.isEmpty()) {
+            return src.replace(dest, stringReverse(dest));
+        }
+        return "Не, так не пойдёт";
+    }
+    public static String indexReverse(String src, int firstIndex, int lastIndex) {
+        if (!src.isEmpty() && lastIndex > firstIndex) {
+            char[] array = src.toCharArray();
+            String temp = "";
+             for(int i = firstIndex; i <= lastIndex; i++) {
+                 temp = temp + array[i];
+             }
+             temp = stringReverse(temp);
+
+             int j = 0;
+
+            for (int i = firstIndex; i <= lastIndex; i++) {
+                array[i] = temp.toCharArray()[j];
+                j++;
+            }
+            return String.valueOf(array);
+        }
+        return "Не, так тоже не катит";
     }
 }
