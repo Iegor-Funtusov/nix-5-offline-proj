922c2c14f6623dccd3065afdee78b3d66218d90f
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-05-16 14:47:57 +0300

    add Main class

---
+++ unit_5_string/src/main/java/ua/davidenko/Main.java
@@ -0,0 +1,9 @@
+package ua.davidenko;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UserOperation.userMethods();
+    }
+}
