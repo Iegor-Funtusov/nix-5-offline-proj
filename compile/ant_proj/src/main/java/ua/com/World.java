44d05b0c7a715c19cff5a55268fc8f6b59451d46
EFlatron <small.fin.murlock@gmail.com>
2021-05-18 00:29:51 +0300

    init commit

--- compile/ant_proj/src/main/java/ua/com/World.java
+++
@@ -1,7 +0,0 @@
-package main.java.ua.com;
-
-public class World {
-    public static void main(String[] args) {
-        System.out.println("World.main");
-    }
-}
---
+++ module_1/control/src/main/java/ua/com/alevel/controlpanel/ThirdLevel.java
@@ -0,0 +1,4 @@
+package ua.com.alevel.controlpanel;
+
+public class ThirdLevel {
+}
