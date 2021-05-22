efa892c0031fe5b6307da5d5fdc71ff11f7b0a8b
Arthur <arthur.sarks@gmail.com>
2021-05-22 20:29:14 +0300

    do 1st dz

---
+++ compile/pr_ant/src/ua/com/nix/Man.java
@@ -0,0 +1,11 @@
+package ua.com.nix;
+import org.apache.commons.lang3.*;
+public class Man{
+
+      public void print(){
+        String s = "Hellow i am MAN";
+        System.out.println(s);
+        s = StringUtils.upperCase(s);
+        System.out.println(s);
+    }
+}
---
+++ compile/pr_maven/src/main/java/ua/com/Hellow.java
@@ -0,0 +1,14 @@
+package ua.com;
+import org.apache.commons.lang3.*;
+import ua.com.nix.*;
+
+public class Hellow {
+
+       public static void main(String[]args){
+       Man man1 = new Man();
+       Animal animal1 = new Animal();
+       man1.print();
+       animal1.print();
+       }
+
+}
