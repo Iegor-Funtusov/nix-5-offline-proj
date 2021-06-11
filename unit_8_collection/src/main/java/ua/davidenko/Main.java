d50490da9b984b68378a8425faebb53440a0700b
Oleksandr Davydenko <saniadavidenko@gmail.com>
2021-06-11 10:13:42 +0300

    add Main class

---
+++ unit_8_collection/src/main/java/ua/davidenko/Main.java
@@ -0,0 +1,8 @@
+package ua.davidenko;
+
+public class Main {
+    public static void main(String[] args) {
+        MathSetTest mathSetTest = new MathSetTest();
+        mathSetTest.test();
+    }
+}
