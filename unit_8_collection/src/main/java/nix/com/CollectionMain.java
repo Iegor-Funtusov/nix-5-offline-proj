9ae4e02809d97adc7efa38bbfc5ab97053be731c
swichbalde <lisnih55@gmail.com>
2021-06-15 14:22:57 +0300

    unit_8 done: mathset

--- unit_8_collection/src/main/java/nix/com/CollectionMain.java
+++ unit_8_collection/src/main/java/nix/com/CollectionMain.java
@@ -6,18 +6,40 @@ import java.util.Arrays;
 
 public class CollectionMain {
     public static void main(String[] args) {
-        MathSet<Integer> mathSet = new MathSet<>();
+        MathSet<Integer> mathSet1 = new MathSet<>();
 
+        Integer[] array = new Integer[20];
         for (int i = 0; i < 20; i++) {
-            mathSet.add(i);
+            mathSet1.add(i);
+            array[i] = i + 10;
         }
 
-        mathSet.join(mathSet);
+        MathSet<Integer> mathSet2 = new MathSet<>(array);
+        MathSet<Integer> mathSet3 = new MathSet<>(mathSet1,mathSet2);
 
-        System.out.println("Join:" + Arrays.toString(mathSet.readAll()));
+        System.out.println("mathSet3 = " + Arrays.toString(mathSet3.toArray()));
+        System.out.println("mathSet2 = " + Arrays.toString(mathSet2.toArray()));
+        System.out.println("mathSet1 = " + Arrays.toString(mathSet1.toArray()));
 
-        mathSet.sortAsc();
+        mathSet1.join(mathSet1, mathSet2);
 
-        System.out.println("Sort: " + Arrays.toString(mathSet.readAll()));
+        System.out.println("Join:" + Arrays.toString(mathSet1.toArray()));
+
+        mathSet1.sortDesc();
+
+        System.out.println("Sort: " + Arrays.toString(mathSet1.toArray()));
+
+        System.out.println("display:" + Arrays.toString(mathSet1.toArray()));
+
+        System.out.println("max " + mathSet1.getMax());
+        System.out.println("id " + mathSet1.get(5));
+        System.out.println("min " + mathSet1.getMin());
+        System.out.println("av " + mathSet1.getAverage());
+        System.out.println("mid " + mathSet1.getMedian());
+        System.out.println("to arr id " + Arrays.toString(mathSet1.toArray(1, 5)));
+
+        mathSet1.clear();
+        System.out.println("mathSet3 = " + Arrays.toString(mathSet1.toArray()));
+
     }
 }
--- unit_8_collection/src/main/java/nix/com/math_set/MathSet.java
+++ unit_8_collection/src/main/java/nix/com/math_set/MathSet.java
@@ -1,9 +1,8 @@
 package nix.com.math_set;
 
-
 public class MathSet <T>{
 
-    private T[] elements;
+    private T[] elements = (T[]) new Object[10];
     private int pos = 0;
 
     public MathSet() {
@@ -14,25 +13,127 @@ public class MathSet <T>{
         elements = (T[]) new Object[size];
     }
 
+    public MathSet(T[] numbers) {
+        newArray(numbers.length);
+        for (int i = 0; i < elements.length; i++) {
+            if (checkElement(numbers[i])) {
+                elements[i] = numbers[i];
+            }
+        }
+        newArray(elements.length - countNull());
+    }
+
+    public MathSet(T[] ... numbers) {
+        int size = 0;
+        for (T[] number : numbers) {
+            size += number.length;
+        }
+        newArray(size);
+        for (T[] number : numbers) {
+            for (int i = 0; i < number.length; i++) {
+                if (checkElement(number[i])) {
+                    elements[i] = number[i];
+                }
+            }
+        }
+        newArray(elements.length - countNull());
+    }
+
+    public MathSet(MathSet numbers) {
+        int size = 0;
+        T[] elementsNew = (T[]) numbers.toArray();
+        for (T number : elementsNew) {
+            size++;
+        }
+        newArray(size);
+        for (int i = 0; i < elementsNew.length; i++) {
+            if (checkElement(elementsNew[i])) {
+                elements[i] = elementsNew[i];
+            }
+        }
+        newArray(elements.length - countNull());
+    }
+
+    public MathSet(MathSet ... numbers) {
+        int size = 0;
+        for (MathSet number : numbers) {
+            T[] elementsNew = (T[]) number.toArray();
+            size += elementsNew.length;
+        }
+        newArray(size);
+        for (MathSet number : numbers) {
+            T[] elementsNew = (T[]) number.toArray();
+            for (int i = 0; i < elementsNew.length; i++) {
+                if (pos >= elements.length) {
+                    break;
+                }
+                if (checkElement(elementsNew[i])) {
+                    elements[pos] = elementsNew[i];
+                    pos++;
+                }
+            }
+        }
+        newArray(elements.length - countNull());
+    }
+
     public void add(T number) {
         if (elements.length <= pos) {
-            newArray((int) (elements.length * 1.5));
+            newArray((int) (elements.length * 1.1));
         }
-        elements[pos] = number;
-        pos++;
+        if (checkElement(number)) {
+            elements[pos] = number;
+            pos++;
+        }
+    }
+
+    public void add(T ... n) {
+        if (elements.length <= pos) {
+            newArray((int) (elements.length * 1.1));
+        }
+        for (T t : n) {
+            if (checkElement(t)) {
+                elements[pos] = t;
+            }
+            pos++;
+        }
     }
 
     public void join(MathSet ms) {
-        T[] elementsNew = (T[]) ms.readAll();
-        newArray((int) ((elements.length + elementsNew.length)));
+        T[] elementsNew = (T[]) ms.toArray();
+        newArray(((elements.length + elementsNew.length)));
         int j = 0;
         for (int i = pos; i < elements.length + elementsNew.length; i++) {
             if (elementsNew.length == j) {
                 break;
             }
-            elements[i] = elementsNew[j];
+            if (checkElement(elementsNew[j])) {
+                elements[i] = elementsNew[j];
+            }
             j++;
         }
+        newArray(elements.length - countNull());
+    }
+
+    public void join(MathSet ... ms) {
+        int size = 0;
+        for (MathSet number : ms) {
+            T[] elementsNew = (T[]) number.toArray();
+            size += elementsNew.length;
+        }
+        newArray(size);
+        for (MathSet number : ms) {
+            T[] elementsNew = (T[]) number.toArray();
+            for (int i = 0; i < elementsNew.length; i++) {
+                if (pos >= elements.length) {
+                    break;
+                }
+                if (checkElement(elementsNew[i])) {
+                    elements[pos] = elementsNew[i];
+                    pos++;
+                }
+            }
+        }
+        newArray(elements.length - countNull());
     }
 
     public void sortAsc() {
@@ -101,7 +202,111 @@ public class MathSet <T>{
         reverseArr();
     }
 
+    public T get(int index) {
+        return elements[index];
+    }
+
+    public T getMax() {
+        T max = elements[0];
+        newArray(elements.length - countNull());
+        for (T element : elements) {
+            try {
+                if ((int) max < (int) element) {
+                    max = element;
+                }
+            } catch(ClassCastException c) {
+                if ((double) max < (double) element) {
+                    max = element;
+                }
+            }
+        }
+        return max;
+    }
+
+    public T getMin() {
+        T min = elements[0];
+        newArray(elements.length - countNull());
+        for (T element : elements) {
+            try {
+                if ((int) min > (int) element) {
+                    min = element;
+                }
+            } catch(ClassCastException c) {
+                if ((double) min > (double) element) {
+                    min = element;
+                }
+            }
+        }
+        return min;
+    }
+
+    public T getAverage() {
+        newArray(elements.length - countNull());
+        T[] sumT = null;
+        try {
+            int sum = 0;
+            for (T element : elements) {
+                sum += (int) element;
+            }
+            MathSet<Integer> intSet = new MathSet<>();
+            intSet.add(sum/elements.length);
+            sumT = (T[]) intSet.toArray();
+        } catch (ClassCastException classCastException) {
+            double sum = 0;
+            for (T element : elements) {
+                sum += (double) element;
+            }
+            MathSet<Double> intSet = new MathSet<>();
+            intSet.add(sum/elements.length);
+            sumT = (T[]) intSet.toArray();
+        }
+        return sumT[0];
+    }
+
+    public T getMedian() {
+        return elements[elements.length / 2];
+    }
+
+    public T[] toArray() {
+        return elements;
+    }
+
+    public T[] toArray(int firstIndex, int lastIndex) {
+        T[] elementsNew = (T[]) new Object[lastIndex - firstIndex];
+
+        for (int i = firstIndex, j = 0; i < lastIndex; j++, i++) {
+            elementsNew[j] = elements[i];
+        }
+        return elementsNew;
+    }
+
+    public MathSet squash(int firstIndex, int lastIndex) {
+        T[] elementsNew = (T[]) new Object[lastIndex - firstIndex];
+
+        for (int i = firstIndex, j = 0; i < lastIndex; j++, i++) {
+            elementsNew[j] = elements[i];
+        }
+        return new MathSet(elementsNew);
+    }
+    
+    public void clear() {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        newArray(elements.length - countNull());
+    }
+
+    public void clear(Number[] numbers) {
+        for (Number number : numbers) {
+            for (int i = 0; i < elements.length; i++) {
+                if (number == elements[i]) {
+                    elements[i] = null;
+                }
+            }
+        }
+    }
     private void reverseArr() {
+        newArray(elements.length - countNull());
         for (int i = 0; i < elements.length / 2; i++) {
             T tmp = elements[i];
             elements[i] = elements[elements.length - i - 1];
@@ -109,23 +314,35 @@ public class MathSet <T>{
         }
     }
 
-    public T[] readAll() {
-        return elements;
-    }
 
     private void newArray(int size) {
         T[] elementsNew = (T[]) new Object[elements.length];
         int i = 0;
         for (T element : elements) {
+            if (i >= elementsNew.length) {
+                break;
+            }
             elementsNew[i] = element;
             i++;
         }
         i = 0;
         elements = (T[]) new Object[size];
         for (T element : elementsNew) {
+            if (i >= elements.length) {
+                break;
+            }
             elements[i] = element;
             i++;
         }
+    }
+
+    private boolean checkElement(T element) {
+        for (T t : elements) {
+            if (t == element) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private int countNull() {
