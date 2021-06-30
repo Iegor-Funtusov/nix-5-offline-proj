cc5704322bfdcf0903aad7f26130b6b598fbcf6a
swichbalde <lisnih55@gmail.com>
2021-06-30 15:44:54 +0300

    module 2 add FindWay.java writer

--- module_2/src/main/java/nix/com/find_way/FindWay.java
+++ module_2/src/main/java/nix/com/find_way/FindWay.java
@@ -1,31 +1,60 @@
 package nix.com.find_way;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
 
 public class FindWay {
 
     public FindWay() {
 
-        File file = new File("input.txt");
+        File inputFile = new File("input.txt");
+        File outputFile = new File("output.txt");
+        try {
+            outputFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         HashMap<Integer, String> nameCities = new HashMap<>();
-        StringBuilder allGraph = new StringBuilder("");
+        StringBuilder allGraph = new StringBuilder();
         int index = 0;
-        int countCities;
+        int start = 0;
+        int end;
+        int count = 0;
 
-        try (FileReader fr = new FileReader(file);
+        try (FileReader fr = new FileReader(inputFile);
+             FileWriter fw = new FileWriter(outputFile);
+             BufferedWriter writer = new BufferedWriter(fw);
              BufferedReader reader = new BufferedReader(fr)) {
             String line = reader.readLine();
-            countCities = Integer.parseInt(line);
             allGraph.append(line).append(" \n ");
             while (line != null) {
                 line = reader.readLine();
-                if (checkEndOfFile(line)) {
+                if (line == null) {
                     break;
+                }
+                if (checkEndOfFile(line)) {
+                    String[] path = line.split(" ");
+                    for (Map.Entry<Integer, String> entry : nameCities.entrySet()) {
+                        if (entry.getValue().equals(path[0])) {
+                            start = entry.getKey();
+                        }
+                        if (entry.getValue().equals(path[1])) {
+                            end = entry.getKey();
+
+                            if (count == 0) {
+                                allGraph = new StringBuilder(allGraph.substring(0, allGraph.length() - 3));
+                                count++;
+                            }
+
+                            int[][] matrix = parseGraphToMatrix(allGraph.toString());
+                            Vector<Integer> pars = new Vector<>();
+                            int[] ds = findCost(matrix, start, pars);
+                            String cost = "" + ds[end] + "\n";
+                            fw.write(cost);
+                        }
+                    }
+                    continue;
                 }
                 try {
                     if (line.split(" ").length == 1) {
@@ -44,43 +73,18 @@ public class FindWay {
         } catch (IOException e) {
             throw new RuntimeException("Wrong input");
         }
-
-        allGraph = new StringBuilder(allGraph.substring(0, allGraph.length() - 3));
-
-        int[][] matrix = parse(allGraph.toString());
-
-        Vector<Integer> pars = new Vector<>();
-        int[] ds = find(matrix, 0, pars);
-        System.out.println("cost: " +  ds[3]);
     }
 
-    public static void show_path(int start, int end, Vector<Integer> pars, int[] ds) {
-        if(end >= ds.length || ds[end] == 1000)
-            return;
-        System.out.println("стоймость пути: " + ds[end]);
-        System.out.println("путь: ");
-
-        Vector<Integer> ps = new Vector<>();
-        for(int i = end; i != start; i = pars.get(i)) // выделяем путь
-            ps.add(i);
-        ps.add(start);
-
-        for(int i = ps.size() - 1; i >= 0; --i)
-            System.out.println(ps.get(i) + "  ");
-        System.out.println();
-        ps.clear();
-    }
-
-    public static int[] find(int[][] m, int start, Vector<Integer> pars) {
-        int[] ds = new int[m.length];
+    public static int[] findCost(int[][] matrix, int start, Vector<Integer> pars) {
+        int[] ds = new int[matrix.length];
         Arrays.fill(ds, 1000);
 
-        boolean[] visit = new boolean[m.length];
-        pars.setSize(m.length);
+        boolean[] visit = new boolean[matrix.length];
+        pars.setSize(matrix.length);
 
         ds[start] = 0;
         int sel;
-        for (int[] ints : m) {
+        for (int[] ints : matrix) {
             sel = -1;
             for (int j = 0; j < ints.length; ++j) {
                 if (!visit[j] && (sel == -1 || ds[j] < ds[sel]))
@@ -91,11 +95,11 @@ public class FindWay {
                 break;
             visit[sel] = true;
 
-            for (int j = 0; j < m[sel].length; ++j) {
-                if (m[sel][j] == 1000)
+            for (int j = 0; j < matrix[sel].length; ++j) {
+                if (matrix[sel][j] == 1000)
                     continue;
-                if ((ds[sel] + m[sel][j]) < ds[j]) {
-                    ds[j] = ds[sel] + m[sel][j];
+                if ((ds[sel] + matrix[sel][j]) < ds[j]) {
+                    ds[j] = ds[sel] + matrix[sel][j];
                     pars.set(j, sel);
                 }
             }
@@ -103,22 +107,22 @@ public class FindWay {
         return ds;
     }
 
-    private int[][] parse(String allGraph) {
-        Scanner sc = new Scanner(allGraph);
-        int n = sc.nextInt();
-        int[][] m = new int[n][n];
-        for (int[] ints : m) {
+    private int[][] parseGraphToMatrix(String allGraph) {
+        Scanner scanner = new Scanner(allGraph);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int[] ints : matrix) {
             Arrays.fill(ints, 1000);
         }
 
-        int i, j, v;
-        while(sc.hasNextInt()){
-            i = sc.nextInt();
-            j = sc.nextInt();
-            v = sc.nextInt();
-            m[i][j] = m[j][i] = v;
+        int i, j, cost;
+        while(scanner.hasNextInt()){
+            i = scanner.nextInt();
+            j = scanner.nextInt();
+            cost = scanner.nextInt();
+            matrix[i][j] = matrix[j][i] = cost;
         }
-        return m;
+        return matrix;
     }
 
 
