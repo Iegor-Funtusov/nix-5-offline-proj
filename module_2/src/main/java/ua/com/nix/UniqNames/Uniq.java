55e01db3bc4f3ab8def79f9659a70982dae5c047
pilyha <illia.popov57@gmail.com>
2021-06-30 14:34:29 +0300

    Update O(n^2) -> O(n)

--- module_2/src/main/java/ua/com/nix/UniqNames/Uniq.java
+++ module_2/src/main/java/ua/com/nix/UniqNames/Uniq.java
@@ -7,27 +7,17 @@ import java.util.List;
 public class Uniq {
 
     public  void run() {
-        List<String> list = new ArrayList<>(Arrays.asList("Антон", "Женя", "Влад", "Женя", "Антон", "Вася","Влад","Олег"));
+        List<String> list = new ArrayList<>(Arrays.asList("Антон", "Женя", "Влад", "Женя", "Антон", "Женя","Влад","Jkty"));
         System.out.println("All names that were input: " + list);
-        boolean unique = true;
+        String unique = "Not a single unique name";
         for (int i = 0; i < list.size(); i++) {
-            unique = true;
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(j).equals(list.get(i))) {
-                    list.set(j,list.get(list.size()-1));
-                    unique = false;
-                    break;
-                }
-            }
-            if (unique) {
-                System.out.println("Uniq symbol in this list: " + list.get(i));
+            if(i==list.indexOf(list.get(i))&&i==list.lastIndexOf(list.get(i)))
+            {
+                unique="Uniq symbol in this list: " + list.get(i);
                 break;
             }
         }
-        if(!unique)
-        {
-            System.out.println("Not a single unique name");
-        }
+        System.out.println(unique);
     }
 }
 
