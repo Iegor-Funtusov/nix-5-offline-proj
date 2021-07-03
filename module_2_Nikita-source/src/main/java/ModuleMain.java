6ab6f6ff5f7c38781d56ca74fdf6ec25a0ea9276
Nikita <logachovnik@gmail.com>
2021-07-04 01:57:21 +0300

    add dijkstra algorithm

--- module_2_Nikita-source/src/main/java/ModuleMain.java
+++ module_2_Nikita-source/src/main/java/ModuleMain.java
@@ -2,12 +2,19 @@ import java.io.IOException;
 
 public class ModuleMain {
     public static void main(String[] args) {
-        try {
-            //WorkWithDates.dateMain();
+        /*try {
+            WorkWithDates.dateMain();
             UniqueName.mainUniqueName();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
+        }*/
+        int[][] matrix = {
+                {0, 1, 3, 300000},
+                {1, 0, 1, 4},
+                {3, 1, 0, 1},
+                {300000, 4, 1, 0}
+        };
+        TransportationTheory tt = new TransportationTheory(4,matrix );
+        System.out.println(tt.dijkstra(0,3));
     }
 }
---
+++ module_2_Nikita-source/src/main/java/TransportationTheory.java
@@ -0,0 +1,40 @@
+import static java.lang.Math.min;
+
+public class TransportationTheory {
+    private int numberOfCities;
+    private int[][] cost;
+
+    public TransportationTheory(int numberOfCities, int[][] cost) {
+        this.numberOfCities = numberOfCities;
+        this.cost = cost;
+    }
+
+    public int dijkstra(int startIndex, int endIndex) {
+        int infinity = 300000;
+        int[] distances = new int[numberOfCities];
+        boolean[] visited = new boolean[numberOfCities];
+        for (int i = 0; i < numberOfCities; i++) {
+            distances[i] = infinity;
+        }
+        distances[startIndex] = 0;
+
+        while (true) {
+            int v = -1;
+            for (int i = 0; i < numberOfCities; i++) {
+                if (!visited[i] && distances[i] < infinity && (v == -1 || distances[v] > distances[i])) {
+                    v = i;
+                }
+            }
+            if (v == -1) {
+                break;
+            }
+            visited[v] = true;
+            for (int i = 0; i < numberOfCities; i++) {
+                if (!visited[i] && cost[v][i] < infinity) {
+                    distances[i] = min(distances[i], distances[v] + cost[v][i]);
+                }
+            }
+        }
+        return distances[endIndex];
+    }
+}
