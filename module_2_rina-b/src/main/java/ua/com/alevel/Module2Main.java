737c7bb167c7e43f83983eb1ad49254493569f4c
rina-b <darina.tsr@gmail.com>
2021-07-04 03:15:42 +0300

    add 2 task module 2

--- module_2_rina-b/src/main/java/ua/com/alevel/Module2Main.java
+++ module_2_rina-b/src/main/java/ua/com/alevel/Module2Main.java
@@ -1,10 +1,10 @@
 package ua.com.alevel;
 
-import java.io.File;
 
 public class Module2Main {
     public static void main(String[] args) {
 
         System.out.println(DateFormatList.formattedDate());
+        UniqueName.firstUniqueName();
     }
 }
---
+++ module_2_rina-b/src/main/java/ua/com/alevel/UniqueName.java
@@ -0,0 +1,38 @@
+package ua.com.alevel;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UniqueName {
+    public static void firstUniqueName() {
+        File file = new File("names.txt");
+        List<String> arrayList = new ArrayList<>();
+        try(FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader)) {
+            String line = reader.readLine();
+            while (line!= null){
+                arrayList.add(line);
+                line = reader.readLine();
+            }
+            String[] arr = arrayList.toArray(new String[0]);
+            Map<String, Long> stringCountMap = Arrays.stream(arr)
+                    .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+            for (String s : arr) {
+                if (stringCountMap.get(s) == 1) {
+                    System.out.println("The first non-duplicate name: " + s);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
